481e3101959048b5b6569a3a2aabb5e898d51c0c
DenZaiyy <denzaiyy@outlook.fr>
2021-11-16 21:54:54 +0100

    test

--- index.js
+++ index.js
@@ -3,8 +3,16 @@ require('dotenv').config()
 const { Client, Collection, Intents } = require('discord.js')
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] })
 
+const BOT_PREFIX = "!d"
+
 client.on('ready', () =>{
     console.log(`Connecter en tant que ${client.user.tag} !`);
+})
+
+client.on("message", msg =>{
+    if(msg.content === `${BOT_PREFIX} + " " + "test"`){
+        msg.reply("this is a test!")
+    }
 })
 
 client.login(process.env.BOT_TOKEN)
