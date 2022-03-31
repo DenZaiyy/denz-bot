7b045126dfddf648a1dcf9eefbde3c51ec7bf794
DenZaiyy <denzaiyy@outlook.fr>
2022-03-31 22:18:56 +0200

    change status bot

--- events/ready.js
+++ events/ready.js
@@ -2,7 +2,8 @@ module.exports = {
 	name: 'ready',
 	once: true,
 	execute(client) {
-		client.user.setActivity('denzaiyy.fr | /help');
+		client.user.setActivity('denzaiyy.fr | /help', { type: 'STREAMING', url: 'http://twitch.tv/denzaiyy' });
+		//client.user.setStatus('idle');
 		console.log(`Connecter en tant que ${client.user.tag}`);
 	},
 };
